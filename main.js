dc46cd2df8e1095a9efb0d926d3806ca8b3c135d
Ionică Bizău <bizauionica@yahoo.com>
2014-01-26 09:25:42 +0200

    greasemonkey header

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
+// ==UserScript==
+// @name        Remove ads
+// @namespace   ...
+// @description This is a small client script that removes the ads from the pages.
+// @include     *
+// @version     1
+// @grant       none
+// ==/UserScript==
+
 // Yahoo! Mail
 if (window.location.host.indexOf("mail.yahoo.com") !== -1) {
-  document.getElementById("theAd").remove()
+    document.getElementById("theAd").remove()
 }
